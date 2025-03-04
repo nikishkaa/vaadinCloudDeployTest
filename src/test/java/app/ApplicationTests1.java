525a74ff0a251c073dab7348a5dfd828b1babdec
nikita <nikita.goutor@gmail.com>
2025-03-04 19:08:05 +0300

    Test

---
+++ src/test/java/app/ApplicationTests1.java
@@ -0,0 +1,13 @@
+package app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests1 {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
